996d577fbeaaf1b0f23cf2a055631c0215af9e0a
haorancui2001 <haorancui2001@gmail.com>
2023-07-06 12:24:06 +1000

    update till episode11

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import "./App.css";
-import { useState, createContext } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
-import { Contact } from "./pages/Contact";
-import { Profile } from  "./pages/Profile";
+import { Form } from "./components/Form";
 
-export const AppContext = createContext();
-
-function App() {
-    const [ username, setUsername ] = useState("JackCui");
-
+function App(){
     return (
         <div className="App">
-            <AppContext.Provider value={{ username, setUsername }}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home username={username} />} />
-                    <Route 
-                        path="/profile" 
-                        element={<Profile />} 
-                    />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="*" element={<h1>Page not found</h1>}/>
-                </Routes>
-            </Router>
-            </AppContext.Provider>
+            <Form />
         </div>
-    ) 
+    );
 }
 
 export default App;
